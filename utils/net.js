42d2830aa558e242d9ed19baac33dc3dfb30541e
lqzh <9134671+lqzhgood@users.noreply.github.com>
2023-06-04 21:18:53 +0800

    ✨ feat(down): 增加默认下载超时时间 10min

--- utils/net.js
+++ utils/net.js
@@ -15,7 +15,7 @@ const axiosDown = axiosBase.create({
         Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
     },
     maxRedirects: 0,
-    // timeout: 60 * 1000,
+    timeout: 10 * 60 * 1000,
 });
 
 async function down(url, absoluteOutFile) {
