d0036469287b5d494bac73aa02c9e57788afdc40
lqzh <9134671+lqzhgood@users.noreply.github.com>
2023-07-22 22:57:52 +0800

    📃 docs(消息): 增加 _文本_自动回复 样本

---
+++ decode/typeHandle/text/tpl/-10000.js
@@ -0,0 +1,67 @@
+const tp = {
+    m: {
+        _id: 1500,
+        extInt: 0,
+        extLong: 1,
+        extStr: '{"zplan_head_effect_msg_flag":"PLAYING","vip_font_effect_id":"0","vip_face_id":"0"}',
+        extraflag: 0,
+        frienduin: '********************',
+        isValid: 1,
+        isread: 1,
+        issend: 0,
+        istroop: 0,
+        longMsgCount: 1,
+        longMsgId: 0,
+        longMsgIndex: 0,
+        msgData: {
+            type: 'Buffer',
+        },
+        msgId: 0,
+        msgUid: '72057595726535256',
+        msgseq: 1688607321,
+        msgtype: -10000,
+        selfuin: '********************',
+        sendFailCode: 0,
+        senderuin: '************************',
+        shmsgseq: -7688,
+        time: 1688607321,
+        uniseq: '7252513215513984373',
+        versionCode: 3,
+        vipBubbleID: 0,
+        mExJsonObject: null,
+    },
+    test: {
+        decryptString: '[自动回复] Error：114514',
+        decryptProtoBuf: '[自动回复] Error：114514',
+        ForwardExtra: {
+            decode: 'index out of range: 18 + 114 > 29',
+        },
+        PicRec: {
+            decode: 'invalid wire type 4 at offset 14',
+        },
+        MessageInfo: {
+            decode: 'index out of range: 18 + 114 > 29',
+        },
+        PttRec: {
+            decode: 'invalid varint encoding',
+        },
+        MarketFaceRec: {
+            decode: 'invalid wire type 4 at offset 14',
+        },
+        ShortVideo: {
+            decode: 'index out of range: 18 + 114 > 29',
+        },
+        Tip: {
+            decode: 'index out of range: 18 + 114 > 29',
+        },
+        RecallInfo: {
+            decode: 'index out of range: 18 + 114 > 29',
+        },
+        Elem: {
+            decode: 'index out of range: 18 + 114 > 29',
+        },
+        Msg: {
+            decode: 'index out of range: 18 + 114 > 29',
+        },
+    },
+};
