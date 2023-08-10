822c5acfd92ea104e91ccc1a9036993ae090d07c
lqzh <9134671+lqzhgood@users.noreply.github.com>
2023-08-10 13:23:52 +0800

    🐞 fix(proto decode): 标准化输出为 Object

--- decode/utils/ddData.js
+++ decode/utils/ddData.js
@@ -18,7 +18,9 @@ function ddProtoBuf(m, p = 'msgData.data', protoDecoder) {
     const buff = decryptProtoBuf(_.get(m, p));
     const obj = protoBufDecode[protoDecoder].decode(buff);
 
-    return obj;
+    // proto 对象  类 Object
+    // 例如 Object.assign 会导致 proto对象 toJSON 丢失  导致 JSON.stringify 输出值不一样
+    return obj.toJSON();
 }
 
 module.exports = {
