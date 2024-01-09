321c599030cf208c84ae16516acb855a20fefbb9
lqzh <9134671+lqzhgood@users.noreply.github.com>
2024-01-09 22:59:38 +0800

    🐞 fix(chatpic): 文件夹不存在的情况

--- decode/typeHandle/image/const.js
+++ decode/typeHandle/image/const.js
@@ -1,10 +1,24 @@
 const fs = require('fs-extra');
 const path = require('path');
 
-const { ASSETS_ROOT_DIR, FILE_WEB_PUBLIC_DIR, FILE_DIR_OUT_DIR, leftNum, rightNum, DIST_DIR } = require('@/config.js');
+const {
+    ASSETS_ROOT_DIR,
+    FILE_WEB_PUBLIC_DIR,
+    FILE_DIR_OUT_DIR,
+    leftNum,
+    rightNum,
+    DIST_DIR,
+} = require('@/config.js');
 
-const IMG_DIR = ['chatraw', 'chatimg', 'chatthumb'];
 const INPUT_DIR_IMAGE = path.join(ASSETS_ROOT_DIR, './chatpic/');
+const IMG_DIR = ['chatraw', 'chatimg', 'chatthumb'];
+
+for (let i = 0; i < IMG_DIR.length; i++) {
+    const p = path.join(INPUT_DIR_IMAGE, IMG_DIR[i]);
+    if (!fs.existsSync(p)) {
+        fs.mkdirpSync(p);
+    }
+}
 
 const DIR_TYPE = 'image';
 const FILE_DIR = path.join(FILE_DIR_OUT_DIR, DIR_TYPE);
