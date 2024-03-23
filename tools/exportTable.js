1915de5020bf3f9c33f2ee74bc7a337c84cf921c
lqzh <9134671+lqzhgood@users.noreply.github.com>
2024-03-23 14:30:07 +0800

    fix(exportTable): 修复文件夹不存在的情况

--- tools/exportTable.js
+++ tools/exportTable.js
@@ -1,10 +1,14 @@
 const path = require('path');
+const fs = require('fs-extra');
+const config = require('../config');
 
 const { exportTable, exportTableName } = require('../decode/utils/db');
 const { Emoticon, EmoticonPackage, EmoticonTab } = require(path.join(
     __dirname,
     '../decode/table/Emoticon'
 ));
+
+fs.mkdirpSync(path.join(config.DIST_DIR_TEMP, './table'));
 
 const { friends, friendMulti } = require('../decode/table/friends');
 
